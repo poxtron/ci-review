91c15a31e5389054485950137f1f0f312fb75a58
Miguel Sirvent <miguel.sirvent@gmail.com>
2020-07-05 16:33:12 -0500

    test 9000

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ if [[ -z "$GH_BOT_TOKEN" ]]; then
     exit 1
 fi
 
+cat $GITHUB_EVENT_PATH
+
+exit 1
+
 # shellcheck disable=SC2164
 cd "$GITHUB_WORKSPACE"
 
@@ -14,21 +18,38 @@ current_version="vip-go-ci-$GOCI_VERSION"
 # getting current version of vip-go-ci
 wget https://github.com/Automattic/vip-go-ci/archive/$GOCI_VERSION.tar.gz
 
-tar -xzvf ${current_version}.tar.gz
+tar -xzvf $GOCI_VERSION.tar.gz
 
 mkdir -p vip-go-ci
 
 mv $current_version/* vip-go-ci
 
+mv $current_version/.* vip-go-ci
+
+rm -r $current_version $GOCI_VERSION.tar.gz
+
+
+cd "$GITHUB_WORKSPACE"/rules
+
+composer install
+
+cd "$GITHUB_WORKSPACE"
+
+COMMIT_ID=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.sha')
+PR_BODY=$(cat "$GITHUB_EVENT_PATH" | jq -r .pull_request.body)
+GITHUB_REPO_NAME=${GITHUB_REPOSITORY##*/}
+GITHUB_REPO_OWNER=${GITHUB_REPOSITORY%%/*}
+
+phpcs_standard="$GITHUB_WORKSPACE"/rules
+
+
+./vip-go-ci/vip-go-ci.php --repo-owner=$GITHUB_REPO_OWNER --repo-name=$GITHUB_REPO_NAME --commit=$COMMIT_ID --token=$GH_BOT_TOKEN --phpcs-path=/home/rtbot/vip-go-ci-tools/phpcs/bin/phpcs --local-git-repo=/home/rtbot/github-workspace --phpcs=true $phpcs_standard
+
 ls -lR
 
 exit 1;
 
-cd "$GITHUB_WORKSPACE"/rules || printf "[\e[0;31mERROR\e[0m]\n"
 
-composer install
-
-mkdir -p "$GITHUB_WORKSPACE"/vip-go-ci
 
 #
 #
